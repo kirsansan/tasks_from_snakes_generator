a76e011b704677879c4a1fc82ed934c525f79aaf
kirsansan <kirill_home@mail.ru>
2023-01-20 20:07:08 +0300

    rebuild base.html add lesson1.html add about.html add styles.css add /between handler

--- app.py
+++ app.py
@@ -13,11 +13,21 @@ from utils.utils import prepare_menu
 app = Flask(__name__)
 
 site_menu: list = []
-prepare_menu(site_menu)
+#prepare_menu(site_menu)
+site_menu = [{"name": "Lesson 1", "url": "/lesson1"},
+             {"name": "Lesson 2", "url": "/lesson2"},
+             {"name": "Lesson 3", "url": "/lesson3"},
+             {"name": "Lesson 4", "url": "/lesson4"},
+             {"name": "Lesson 5", "url": "/lesson5"}]
+lesson1_menu = [{"name": "Hello", "url": "/user/Gleb"},
+                {"name": "Task 1 letter", "url": "/letter/y"},
+                {"name": "Task 2 find", "url": "/find/?letter=Q"},
+                {"name": "Task 3 check", "url": "/check/B/Bravo"},
+                {"name": "Task 4 between ", "url": "/between/?from=<letter>&to=<letter>"}]
 
 @app.route('/')
 def index():
-    return render_template('base.html', title="ONE", menu=site_menu)
+    return render_template('index.html', title="ONE", menu=site_menu, submenu=lesson1_menu)
 
 # def hello_world():  # put application's code here
 #     word = request.args.get('num')
@@ -30,11 +40,18 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('base.html', menu=site_menu)
+    return render_template('about.html', menu=site_menu)
+
+@app.route('/lesson1')
+def lesson1():
+    return render_template('lesson1.html', menu=site_menu, submenu=lesson1_menu)
+
+
 
 @app.route('/test')
 def hello_one_more_time():
-    return '<h1>Hello World! Do you want to have some test? </h1>'
+    return render_template('index.html', menu=site_menu, adout='<h1>Hello World! Do you want to have some test? </h1>')
+    # return '<h1>Hello World! Do you want to have some test? </h1>'
 
 @app.route('/user/<name>')
 def user(name):
@@ -62,6 +79,15 @@ def is_char_associate_word(letter, word):
     if (str_letter in alphabet) and alphabet[str_letter] == str(word):
         return "True"
     return "False"
+
+@app.route('/between/')
+def find_between():
+    """Handler for  'between/?from=<letter>&to=<letter>'
+    """
+    char_from = str(request.args.get('from')).upper()
+    char_to = str(request.args.get('to')).upper()
+
+    return
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
